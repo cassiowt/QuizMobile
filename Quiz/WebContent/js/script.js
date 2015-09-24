bfbf3a1d4ce595c7eb32a84c5c832601797811c7
Cassio Trindade <cassiowt@gmail.com>
2015-09-24 14:08:57 -0300

    Acerto bug no switch case
    Signed-off-by: Cassio Trindade <cassiowt@gmail.com>

--- Quiz/WebContent/js/script.js
+++ Quiz/WebContent/js/script.js
@@ -9,30 +9,30 @@
  **/
 
 /*!
-*  Função perguntas() é responsável por imprimir as perguntas na pagina gravar a resposta na memória.
-*  
-*  Temos uma conjunto de series finitas que são escolidas atraves de um numero randomico,
-*  passado esse para o Switch Case é escolida a sequencia a ser impressa na tela.
-*  
-*/
+ *  Função perguntas() é responsável por imprimir as perguntas na pagina gravar a resposta na memória.
+ *  
+ *  Temos uma conjunto de series finitas que são escolidas atraves de um numero randomico,
+ *  passado esse para o Switch Case é escolida a sequencia a ser impressa na tela.
+ *  
+ */
 
 function perguntas() {
 	document.getElementById('resposta').value = "";
-	document.getElementById('resultado').innerHTML ="";
-	var p = getRandomInt(5,0);
-	
+	document.getElementById('resultado').innerHTML = "";
+	var p = getRandomInt(5, 0);
+
 	var serie;
 	var resposta;
-	
+
 	var seriePi = new Array();
 	var serieFibonacci = new Array();
 	var serieSquares = new Array();
 	var seriePrimes = new Array();
-	
-	var seriePi = [ 3, 1, 4, 1, 5," " ]; // pi
-	var serieFibonacci = [ 1, 1, 2, 3, 5," " ]; // Fibonacci
-	var serieSquares = [ 1, 2, 4, 8, 16," " ]; // Squares
-	var seriePrimes = [ 2, 3," ", 7, 11  ]; // Prime
+
+	var seriePi = [ 3, 1, 4, 1, 5, " " ]; // pi
+	var serieFibonacci = [ 1, 1, 2, 3, 5, " " ]; // Fibonacci
+	var serieSquares = [ 1, 2, 4, 8, 16, " " ]; // Squares
+	var seriePrimes = [ 2, 3, " ", 7, 11 ]; // Prime
 
 	switch (p) {
 	case 1:
@@ -53,36 +53,38 @@ function perguntas() {
 		break;
 	}
 
-	document.getElementById('pergunta').innerHTML = serie.toString().replace(" ","...");
+	document.getElementById('pergunta').innerHTML = serie.toString().replace(
+			" ", "...");
 
 }
 
-/*!
- *  Função quiz() é responsável por validar as pespostas.
- *  
- *  Busca na variavel "resposta" qual é a que foi inputada na pagina html e utiliza um
- *  Switch Case para validar a resposta orreta.
- *  
- *  A cada resposta certa é adicionado 10 pontos  no Score do jogo, Pontuação fixa.
- *  que é armazenada na variável "scoreAtualizado".
- *  
- *  Quando o usuario acerta é impresso na pagina uma mensagem de sucesso com o nome da sequencia:
- *  		"Voce acertou.  Sequencia SEUENCIA", com a cora AZUL.
- *  Quando o usuario erra é impresso na pagina uma mensagem de erro com uma dica da sequencia:
- *  		"Voce ERROU.<br>  Dica:  SEUENCIA", com a cor VERMELHA.
- *  
+/*
+ * ! Função quiz() é responsável por validar as pespostas.
+ * 
+ * Busca na variavel "resposta" qual é a que foi inputada na pagina html e
+ * utiliza um Switch Case para validar a resposta orreta.
+ * 
+ * A cada resposta certa é adicionado 10 pontos no Score do jogo, Pontuação
+ * fixa. que é armazenada na variável "scoreAtualizado".
+ * 
+ * Quando o usuario acerta é impresso na pagina uma mensagem de sucesso com o
+ * nome da sequencia: "Voce acertou. Sequencia SEUENCIA", com a cora AZUL.
+ * Quando o usuario erra é impresso na pagina uma mensagem de erro com uma dica
+ * da sequencia: "Voce ERROU.<br> Dica: SEUENCIA", com a cor VERMELHA.
+ * 
  */
 function quiz() {
 
 	var resposta = localStorage.getItem("resposta")
+
 	var valorPonto = 10;
 	var score = localStorage.getItem("scoreAtualizado", score);
-	
+
 	if (score == null)
 		score = 0
 	else
 		score = score;
-	
+
 	var respostaCerta = document.getElementById('resposta').value;
 
 	switch (resposta) {
@@ -99,36 +101,43 @@ function quiz() {
 		valorResposta = 5
 		break;
 	}
-
 	if (valorResposta == respostaCerta) {
+		// DIGITEM AS LINHAS ABAIXO
 		score = parseInt(score) + parseInt(valorPonto);
 		document.getElementById('valorPontos').innerHTML = score;
-		document.getElementById('resultado').innerHTML = "Voce acertou.<br> Sequencia " + resposta;
+		// *******
+
+		document.getElementById('resultado').innerHTML = "Voce acertou.<br> Sequencia "
+				+ resposta;
 		document.getElementById('resultado').style.color = "blue";
+
+		// DIGITEM A LINHA ABAIXO
 		localStorage.setItem("scoreAtualizado", score);
+
 	} else {
-		document.getElementById('resultado').innerHTML = "Voce ERROU.<br>  Dica: " + resposta;
-		document.getElementById('resultado').style.color = "red";		
+		document.getElementById('resultado').innerHTML = "Voce ERROU.<br>  Dica: "
+				+ resposta;
+		document.getElementById('resultado').style.color = "red";
 	}
 }
 
 
-
-/*!
-*  Função zerarPontuacao() apaga todos os campos da tela e limpa da memoria o score de pontuação.
-*/
+/*
+ * ! Função zerarPontuacao() apaga todos os campos da tela e limpa da memoria o
+ * score de pontuação.
+ */
 function zerarPontuacao() {
-	localStorage.setItem("scoreAtualizado", 0);
-	document.getElementById('valorPontos').innerHTML = "0";
-	document.getElementById('resposta').value = "";
-	document.getElementById('resultado').innerHTML = "";
-	
+localStorage.setItem("scoreAtualizado", 0);
+document.getElementById('valorPontos').innerHTML = "0";
+document.getElementById('resposta').value = "";
+document.getElementById('resultado').innerHTML = "";
+
 }
 
-/*!
-*  Função getRandomInt() gera um numero randomico no intervalo dos parametros inteiros
-*  min: minimo e max: maximo.
-*/
+/*
+ * ! Função getRandomInt() gera um numero randomico no intervalo dos parametros
+ * inteiros min: minimo e max: maximo.
+ */
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+return Math.floor(Math.random() * (max - min + 1)) + min;
 }
